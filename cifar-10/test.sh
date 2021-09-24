bffa1fe76067d5395293ae99fa141660dea9c65a
JokerYan <770792058@qq.com>
2021-09-24 21:21:14 +0800

    adjust data dir

--- cifar-10/test.sh
+++ cifar-10/test.sh
@@ -1,1 +1,1 @@
-python main.py --todo test --data_root ../data/cifar-10-batches-py -e 0.314 -p 'l2' --load_checkpoint 7step_2alpha_0.1lr.pth
+python main.py --todo test --data_root ../../data/ -e 0.314 -p 'l2' --load_checkpoint 7step_2alpha_0.1lr.pth
