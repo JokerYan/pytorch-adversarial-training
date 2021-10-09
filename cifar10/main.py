3fe62aad9def36ae36be7695aa41f9f8e50a707f
JokerYan <770792058@qq.com>
2021-10-09 20:09:23 +0800

    add normal acc to output

--- cifar10/main.py
+++ cifar10/main.py
@@ -142,6 +142,7 @@ class Trainer():
         total_adv_acc = 0.0
         total_post_acc = 0.0
         total_neighbour_acc = 0.0
+        total_post_normal_acc = 0.0
 
         train_loaders_by_class = get_train_loaders_by_class(args.data_root, 128)
 
@@ -177,10 +178,19 @@ class Trainer():
                     post_acc = evaluate(post_pred.cpu().numpy(), label.cpu().numpy(), 'sum')
                     total_post_acc += post_acc
 
+                    # post normal
+                    post_model, original_class, neighbour_class, loss_list, acc_list, neighbour_delta = \
+                        post_train(model, data, self.attack, train_loaders_by_class, args)
+                    post_normal_output = post_model(data, _eval=True)
+                    post_normal_pred = torch.max(post_normal_output, dim=1)[1]
+                    post_normal_acc = evaluate(post_normal_pred.cpu().numpy(), label.cpu().numpy(), 'sum')
+                    total_post_normal_acc += post_normal_acc
+
                     total_neighbour_acc += 1 if int(label) == int(original_class) or int(label) == int(neighbour_class) else 0
 
-                    print("Batch {}: adv: {:.4f}\tpost: {:.4f}\tneigh: {:.4f}".format(num-1, total_adv_acc / num, total_post_acc / num, total_neighbour_acc / num))
-                    print("label: {}\toriginal: {}\tneighbour: {}".format(int(label), int(adv_pred), int(neighbour_class)))
+                    print("Batch {}: adv: {:.4f}\tpost adv: {:.4f}\t post normal: {:.4f}\tneigh: {:.4f}".format(
+                        num-1, total_adv_acc / num, total_post_acc / num, total_post_normal_acc / num, total_neighbour_acc / num))
+                    # print("label: {}\toriginal: {}\tneighbour: {}".format(int(label), int(adv_pred), int(neighbour_class)))
                 else:
                     total_adv_acc = -num
 
