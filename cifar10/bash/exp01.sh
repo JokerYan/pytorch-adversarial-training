30d5da7356982e93182e7e8c2a6b3c1f31ba6649
JokerYan <770792058@qq.com>
2021-11-24 17:23:41 +0800

    add test exp01.sh

---
+++ cifar10/bash/exp01.sh
@@ -0,0 +1,18 @@
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+CUDA_VISIBLE_DEVICES=0 python main.py \
+  --todo test \
+  --data_root ../../data/ \
+  --batch_size 1 \
+  -e 0.0314 \
+  -a 0.03921 \
+  -p 'linf' \
+  --load_checkpoint 7step_2alpha_0.1lr.pth \
+  --pt-data ori_neigh \
+  --pt-method adv \
+  --adv-dir na \
+  --neigh-method untargeted \
+  --pt-iter 50 \
+  --pt-lr 0.001 \
+  --att-iter 40 \
+  --att-restart 1 \
+  --log-file logs/log_exp01_${TIMESTAMP}.txt
--- cifar10/src/argument.py
+++ cifar10/src/argument.py
@@ -18,8 +18,8 @@ def parser():
         help='maximum perturbation of adversaries (4/255=0.0157)')
     parser.add_argument('--alpha', '-a', type=float, default=0.00784, 
         help='movement multiplier per iteration when generating adversarial examples (2/255=0.00784)')
-    parser.add_argument('--k', '-k', type=int, default=10, 
-        help='maximum iteration when generating adversarial examples')
+    # parser.add_argument('--k', '-k', type=int, default=10,
+    #     help='maximum iteration when generating adversarial examples')
 
     parser.add_argument('--batch_size', '-b', type=int, default=128, help='batch size')
     parser.add_argument('--max_epoch', '-m_e', type=int, default=200, 
@@ -48,14 +48,14 @@ def parser():
     parser.add_argument('--neigh-method', default='untargeted', choices=['untargeted', 'targeted'], type=str)
     parser.add_argument('--pt-iter', default=50, type=int)
     parser.add_argument('--pt-lr', default=0.001, type=float)
-    # parser.add_argument('--att-iter', default=20, type=int)
+    parser.add_argument('--att-iter', default=20, type=int)
     parser.add_argument('--att-restart', default=1, type=int)
     parser.set_defaults(blackbox=False, type=bool)
     parser.add_argument('--blackbox', dest='blackbox', action='store_true')
     parser.add_argument('--log-file', default='logs/default.log', type=str)
 
     args = parser.parse_args()
-    setattr(args, 'att_iter', args.k)
+    setattr(args, 'k', args.att_iter)
     if args.adv_dir != 'na':
         assert args.pt_method == 'dir_adv'
     if args.pt_method == 'dir_adv':
