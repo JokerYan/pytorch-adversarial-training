8bcbcf1e57f9daabf48059aa2b411bdbc9cd36c1
JokerYan <770792058@qq.com>
2021-12-05 14:29:21 +0800

    update test.sh

--- mnist/bash/test.sh
+++ mnist/bash/test.sh
@@ -1,5 +1,5 @@
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=4 python main.py \
   --todo test \
   --data_root ../../data/ \
   --batch_size 1 \
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
   -p 'linf' \
   --load_checkpoint checkpoint/mnist_/checkpoint_56000.pth \
   --pt-data ori_neigh \
-  --pt-method adv \
-  --adv-dir na \
+  --pt-method dir_adv \
+  --adv-dir both \
   --neigh-method untargeted \
   --pt-iter 50 \
-  --pt-lr 0.001 \
+  --pt-lr 0.003 \
   --att-iter 40 \
   --att-restart 1 \
   --log-file logs/log_test_${TIMESTAMP}.txt
