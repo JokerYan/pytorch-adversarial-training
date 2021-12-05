558de317beb094fbf9539a63df8c850a652082fb
JokerYan <770792058@qq.com>
2021-12-05 15:05:08 +0800

    use adv pt in test

--- mnist/bash/test.sh
+++ mnist/bash/test.sh
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=6 python main.py \
   -p 'linf' \
   --load_checkpoint checkpoint/mnist_/checkpoint_56000.pth \
   --pt-data ori_neigh \
-  --pt-method normal \
+  --pt-method adv \
   --adv-dir na \
   --neigh-method untargeted \
   --pt-iter 50 \
-  --pt-lr 0.003 \
+  --pt-lr 0.001 \
   --att-iter 40 \
   --att-restart 1 \
   --log-file logs/log_test_${TIMESTAMP}.txt
