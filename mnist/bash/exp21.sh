88b65f5983ffbd53ed72cf1d70bb900d1555e3f4
JokerYan <770792058@qq.com>
2021-12-09 09:08:56 +0800

    update exp21 with new checkpoint

---
+++ mnist/bash/exp21.sh
@@ -0,0 +1,18 @@
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+CUDA_VISIBLE_DEVICES=1 python main.py \
+  --todo test \
+  --data_root ../../data/ \
+  --batch_size 1 \
+  -e 0.3 \
+  -a 0.01 \
+  -p 'linf' \
+  --load_checkpoint checkpoint/mnist_2/checkpoint_56000.pth \
+  --pt-data ori_neigh \
+  --pt-method pgd \
+  --adv-dir na \
+  --neigh-method untargeted \
+  --pt-iter 50 \
+  --pt-lr 0.01 \
+  --att-iter 40 \
+  --att-restart 1 \
+  --log-file logs/log_exp21_${TIMESTAMP}.txt
